8b411d83a679d57abb323413175dd1f9f532efa9
Koki Sakai <likuor0528@gmail.com>
2022-08-16 19:20:55 -0700

    Add css on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { useState, useCallback, useEffect } from 'react';
 
-const WrapperNavbar = styled.nav`
+type Props = {
+  isToggle: boolean;
+};
+
+const WrapperNavbar = styled.nav<Props>`
   margin: 0 auto;
   width: 100%;
   margin-top: 13.5vw;
@@ -12,91 +16,175 @@ const WrapperNavbar = styled.nav`
   transform: skewY(15deg);
 `;
 
-const FlexWrapper = styled.div`
-  transform: skew(-15deg);
-  height: 70px;
-  margin: 0 auto;
-  width: 90%;
+const Ul = styled.ul<Props>`
   display: flex;
-  justify-content: center;
+  flex-wrap: wrap;
+  justify-content: space-between;
   align-items: center;
-`;
-
-const FlexChildrenLogo = styled.div`
-  width: 150px;
-  font-size: small;
-
-  @media (min-width: 768px) {
-    font-size: medium;
-  }
-`;
-
-const FlexChildrenList = styled.ul`
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-  font-size: small;
-
-  @media (min-width: 768px) {
-    font-size: medium;
-  }
-`;
-
-const LiWrapper = styled.li`
   list-style: none;
+  padding: 3vw;
+  margin: 0 auto;
+  transition: transform 0.4s ease-in-out;
+  transform: ${(props) => (props.isToggle ? '' : 'translate(0px, 1px)')};
+
+  h1 {
+    transform: skew(-15deg);
+    font-size: large;
+  }
+
+  @media (min-width: 768px) {
+    align-items: flex-start;
+    flex-wrap: nowrap;
+    background: none;
+    padding: 1vw;
+
+    li:first-child {
+      > h1 {
+        padding: 15px 5px;
+      }
+      flex-grow: 1;
+    }
+  }
+`;
+
+const LiWrapper = styled.li<Props>`
+  width: 100%;
+  text-align: center;
   padding: 0 1vw;
+  display: ${(props) => (props.isToggle ? 'none' : 'block')};
+  border-bottom: 1px solid rgba(255, 83, 61, 0.3);
+  :last-child {
+    border-bottom: none;
+  }
+
+  > a {
+    display: block;
+    transform: skew(-15deg);
+    padding: 15px 5px;
+  }
+
+  :hover {
+    transition: transform 0.4s ease-in-out;
+    transform: skew(195deg);
+  }
+
+  @media (min-width: 768px) {
+    font-size: large;
+    border-bottom: none;
+    order: 1;
+    position: relative;
+    display: block;
+    width: auto;
+  }
+`;
+
+const Toggle = styled.li<Props>`
+  height: 20px;
+  width: 26px;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  span {
+    display: block;
+    height: 2px;
+    width: 100%;
+    border-radius: 10px;
+    background: #fff;
+
+    :nth-of-type(1) {
+      transform-origin: 0% 0%;
+      transition: transform 0.4s ease-in-out;
+      transform: ${(props) => (props.isToggle ? '' : 'skew(135deg,45deg)')};
+    }
+
+    :nth-of-type(2) {
+      transition: transform 0.2s ease-in-out;
+      transform: ${(props) => (props.isToggle ? '' : 'scaleY(0)')};
+    }
+
+    :nth-of-type(3) {
+      transform-origin: 0% 100%;
+      transition: transform 0.4s ease-in-out;
+      transform: ${(props) => (props.isToggle ? '' : 'skew(-135deg,-45deg)')};
+    }
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
 `;
 
 const Navbar: FC = () => {
-  const [isHeaderShown, setIsHeaderShown] = useState(true);
-  const [lastPosition, setLastPosition] = useState(0);
-  const headerHeight = 20;
+  const [isToggleShown, setIsToggleShown] = useState(true);
 
-  const scrollEvent = useCallback(() => {
-    const offset = window.pageYOffset;
+  // const [isHeaderShown, setIsHeaderShown] = useState(true);
+  // const [lastPosition, setLastPosition] = useState(0);
+  // const headerHeight = 20;
 
-    if (offset > headerHeight) {
-      setIsHeaderShown(false);
-    } else {
-      setIsHeaderShown(true);
-    }
+  // const scrollEvent = useCallback(() => {
+  //   const offset = window.pageYOffset;
 
-    if (offset < lastPosition) {
-      setIsHeaderShown(true);
-    }
+  //   if (offset > headerHeight) {
+  //     setIsHeaderShown(false);
+  //   } else {
+  //     setIsHeaderShown(true);
+  //   }
 
-    setLastPosition(offset);
-  }, [lastPosition]);
+  //   if (offset < lastPosition) {
+  //     setIsHeaderShown(true);
+  //   }
 
-  useEffect(() => {
-    window.addEventListener('scroll', scrollEvent);
+  //   setLastPosition(offset);
+  // }, [lastPosition]);
 
-    return () => {
-      window.removeEventListener('scroll', scrollEvent);
-    };
-  }, [scrollEvent]);
+  // useEffect(() => {
+  //   window.addEventListener('scroll', scrollEvent);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', scrollEvent);
+  //   };
+  // }, [scrollEvent]);
+
+  const handle = () => {
+    console.log(isToggleShown);
+    return setIsToggleShown(!isToggleShown);
+  };
 
   return (
     <>
-      <WrapperNavbar>
-        <FlexWrapper>
-          <FlexChildrenLogo>
-            <Link to='/'>
-              <h1>Koki Sakai</h1>
+      <WrapperNavbar isToggle={isToggleShown}>
+        <Ul isToggle={isToggleShown}>
+          <li>
+            <h1>
+              <Link to='/'>Koki Sakai</Link>
+            </h1>
+          </li>
+          <Toggle isToggle={isToggleShown} onClick={handle}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </Toggle>
+
+          <LiWrapper isToggle={isToggleShown}>
+            <Link to='/about' onClick={handle}>
+              About
             </Link>
-          </FlexChildrenLogo>
-          <FlexChildrenList>
-            <Link to='/about'>
-              <LiWrapper>About</LiWrapper>
+          </LiWrapper>
+
+          <LiWrapper isToggle={isToggleShown}>
+            <Link to='/work' onClick={handle}>
+              Work
             </Link>
-            <Link to='/work'>
-              <LiWrapper>Work</LiWrapper>
+          </LiWrapper>
+
+          <LiWrapper isToggle={isToggleShown}>
+            <Link to='/contact' onClick={handle}>
+              Contact
             </Link>
-            <Link to='/contact'>
-              <LiWrapper>Contact</LiWrapper>
-            </Link>
-          </FlexChildrenList>
-        </FlexWrapper>
+          </LiWrapper>
+        </Ul>
       </WrapperNavbar>
     </>
   );
