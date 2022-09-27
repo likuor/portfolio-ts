e749ebd7dc92f37dadcac8379bc3a43456f6fce2
Koki Sakai <likuor0528@gmail.com>
2022-09-27 15:36:57 -0700

    Edi resume

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -313,7 +313,7 @@ const About: FC = () => {
                 <span>2020 - 2021, Tokyo, Japan</span>
               </p>
               <a
-                href='https://drive.google.com/file/d/1t4rHe0ORrIwyJURvCdflU4b1r2hnpC0h/view?usp=sharing'
+                href='https://drive.google.com/file/d/1i_M9CujplkYIEz5Mhleu5MP752FWhf0b/view?usp=sharing'
                 target='_blank'
                 rel='noopener noreferrer'
               >
